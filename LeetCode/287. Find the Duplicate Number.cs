c0a7422d1ccd848b57ba8dc21da4fd465b22ab89
danielclas <danielclas@outlook.es>
2020-06-07 19:43:27 -0300

    Ejercicios

---
+++ LeetCode/287. Find the Duplicate Number.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int FindDuplicate(int[] nums) {
+        
+        for(int i=0 ; i<nums.Length-1 ; i++){
+            for(int j=i+1 ; j<nums.Length ; j++){
+                if(nums[i]==nums[j]) return nums[i];
+            }
+        }
+        
+        return -1;
+    }
+}
