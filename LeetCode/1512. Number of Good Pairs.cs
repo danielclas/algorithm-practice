1e00446f37c3e6e42ca2745e2f80de701c43c403
danielclas <danielclas@outlook.es>
2020-09-01 21:46:08 -0300

    Ejercicios

--- LeetCode/1512. Number of Good Pairs.cs
+++ LeetCode/1512. Number of Good Pairs.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int NumIdenticalPairs(int[] nums) {
         
-        int count = 0;
+        var count = 0;
         
         for(int i=0 ; i<nums.Length-1 ; i++){
             for(int j=i+1 ; j<nums.Length ; j++){
--- push.bat
+++ push.bat
@@ -1,5 +1,7 @@
 echo off
 cls
+git config user.name 'danielclas'
+git config user.email 'danielclas@outlook.es'
 git add .
 git commit -m "Ejercicios"
 git push
