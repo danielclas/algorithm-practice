acc7e26d35587a90233c8a2258901f3580d9c449
danielclas <danielclas@outlook.es>
2021-02-24 09:51:16 -0300

    Ejercicios

---
+++ AlgoExpert/First Duplicate.js
@@ -0,0 +1,15 @@
+function firstDuplicateValue(array) {
+	
+	for(let i = 0 ; i < array.length ; i++){
+		let index = Math.abs(array[i]) - 1;
+		
+		if(array[index] < 0) return index + 1;
+	
+		array[index] = array[index] * -1;
+	}
+	
+  return -1;
+}
+
+// Do not edit the line below.
+exports.firstDuplicateValue = firstDuplicateValue;
