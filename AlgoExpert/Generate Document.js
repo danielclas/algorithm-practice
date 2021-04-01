30ee340d0edd1266916a75defa756112cfffdb92
danielclas <danielclas@outlook.es>
2021-04-01 20:11:30 -0300

    Ejercicios

---
+++ AlgoExpert/Generate Document.js
@@ -0,0 +1,22 @@
+function generateDocument(characters, document) {
+  
+	let map = {};
+	
+	for(let c of characters){
+		if(map[c]) map[c]++;
+		else map[c] = 1;
+	}
+	
+	for(let c of document){
+		if(!map[c] || map[c] == 0){
+			return false;
+		}else{
+			map[c]--;
+		}
+	}
+	
+  return true;
+}
+
+// Do not edit the line below.
+exports.generateDocument = generateDocument;
