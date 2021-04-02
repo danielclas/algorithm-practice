a98b910bf60c3dcb654ea72432d85352218a6da6
danielclas <danielclas@outlook.es>
2021-04-02 16:18:23 -0300

    Ejercicios

---
+++ AlgoExpert/Sorted Square Array.js
@@ -0,0 +1,25 @@
+function sortedSquaredArray(array) {
+	
+	const arr = new Array(array.length).fill(0);
+	let smallerIndex = 0, greaterIndex = array.length - 1;
+	
+	for(let i = array.length - 1 ; i >= 0 ; i--){
+		let smallerValue = array[smallerIndex];
+		let greaterValue = array[greaterIndex];
+		
+		if(Math.abs(smallerValue) > Math.abs(greaterValue)){
+			arr[i] = smallerValue * smallerValue;
+			smallerIndex++;
+		}else{
+			arr[i] = greaterValue * greaterValue;
+			greaterIndex--;
+		}
+	}
+	
+	return arr;
+	
+  //return array.map(b => b * b).sort((j,k) => j-k);
+}
+
+// Do not edit the line below.
+exports.sortedSquaredArray = sortedSquaredArray;
