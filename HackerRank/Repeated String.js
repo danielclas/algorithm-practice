d57bcc1496bdb6b2c0bf75f7aaf9b09165ad4b7f
danielclas <danielclas@outlook.es>
2021-06-13 20:50:58 -0300

    Ejercicios

---
+++ HackerRank/Repeated String.js
@@ -0,0 +1,12 @@
+function repeatedString(s, n) {
+    
+    const count = [...s].filter(c => c == 'a').length;
+    const mod = n % s.length;
+    const total = count * Math.floor((n / s.length));
+    
+    if(mod === 0) return total;
+    
+    return total + ([...s].slice(0, mod).filter(c => c == 'a').length);
+    //s = abcac, n = 10
+
+}
